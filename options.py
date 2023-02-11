afcbcd99bb299c77737955c323730bc0c62f44b4
Antonina Alexeeva <alekseevatonya@yandex.ru>
2023-02-11 23:16:37 +0400

    Change for cyrillic generation

--- options.py
+++ options.py
@@ -1,23 +1,25 @@
 import argparse
+# TODO: add help for the parameters
+
 
 def get_parser_main_model():
     parser = argparse.ArgumentParser()
-    # basic parameters training related
+    # TODO: basic parameters training related
     parser.add_argument('--model_name', type=str, default='main_model', choices=['main_model', 'neural_raster'], help='current model_name')
     parser.add_argument('--bottleneck_bits', type=int, default=128, help='latent code number of bottleneck bits')
-    parser.add_argument('--char_categories', type=int, default=52, help='number of glyphs, original is 52')
-    parser.add_argument('--ref_nshot', type=int, default=4, help='reference number')    
+    parser.add_argument('--char_categories', type=int, default=99, help='number of glyphs, original is 52, cyrillic is 99')
+    parser.add_argument('--ref_nshot', type=int, default=52, help='reference number')    
     parser.add_argument('--in_channel', type=int, default=1, help='input image channel')
     parser.add_argument('--out_channel', type=int, default=1, help='output image channel')
-    parser.add_argument('--batch_size', type=int, default=64, help='batch size')
-    parser.add_argument('--image_size', type=int, default=128, help='image size, must be 2**n, 64 and 128 tested')
+    parser.add_argument('--batch_size', type=int, default=16, help='batch size')
+    parser.add_argument('--image_size', type=int, default=64, help='image size')
     parser.add_argument('--image_size_sr', type=int, default=256, help='image size for super resolution')
-    parser.add_argument('--max_seq_len', type=int, default=51, help='maximum length of sequence')
+    parser.add_argument('--max_seq_len', type=int, default=51, help='maximum length of sequence, original is 51')
     parser.add_argument('--seq_feature_dim', type=int, default=10,
                         help='feature dim (like vocab size) of one step of sequence feature')
     # experiment related
     parser.add_argument('--init_epoch', type=int, default=0, help='init epoch')
-    parser.add_argument('--n_epochs', type=int, default=2000, help='number of epochs')
+    parser.add_argument('--n_epochs', type=int, default=1001, help='number of epochs')
     parser.add_argument('--lr', type=float, default=0.0002, help='learning rate')
     parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
     parser.add_argument('--multi_gpu', type=bool, default=False)
@@ -26,7 +28,7 @@ def get_parser_main_model():
                         help='how to read the data, *dirs* consumes much less memory')
     parser.add_argument('--data_root', type=str, default='data/vecfont_dataset_dirs')
     parser.add_argument('--ckpt_freq', type=int, default=25, help='save checkpoint frequency of epoch')
-    parser.add_argument('--sample_freq', type=int, default=200, help='sample train output of steps')
+    parser.add_argument('--sample_freq', type=int, default=400, help='sample train output of steps')
     parser.add_argument('--val_freq', type=int, default=1000, help='sample validate output of steps')
     parser.add_argument('--beta1', type=float, default=0.9, help='beta1 of Adam optimizer')
     parser.add_argument('--beta2', type=float, default=0.999, help='beta2 of Adam optimizer')
@@ -34,7 +36,7 @@ def get_parser_main_model():
     parser.add_argument('--weight_decay', type=float, default=0.0, help='weight decay')
     parser.add_argument('--tboard', type=bool, default=True, help='whether use tensorboard to visulize loss')
     parser.add_argument('--test_sample_times', type=int, default=20, help='the sample times when testing')
-    parser.add_argument('--nr_ckpt_num', type=int, default=1000, 
+    parser.add_argument('--nr_ckpt_num', type=int, default=700, 
                         help='the checkpoint id of neural rasterizer when training main model')
     # loss weight
     parser.add_argument('--kl_beta', type=float, default=0.01, help='latent code kl loss beta')
